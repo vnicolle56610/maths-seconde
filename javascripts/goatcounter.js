9237dd1a6b216cc9263801c30b218dfe61d2b085
Vincent Legendre <vnicolle56@gmail.com>
2026-07-10 17:45:42 +0200

    Deployed 2b53620 with MkDocs version: 1.6.1

---
+++ javascripts/goatcounter.js
@@ -0,0 +1,9 @@
+// Compteur de visites GoatCounter (sans cookie ni donnée personnelle).
+// Tableau de bord : https://maths-vnicolle.goatcounter.com
+(function () {
+  var script = document.createElement("script");
+  script.async = true;
+  script.src = "https://gc.zgo.at/count.js";
+  script.dataset.goatcounter = "https://maths-vnicolle.goatcounter.com/count";
+  document.head.appendChild(script);
+})();
